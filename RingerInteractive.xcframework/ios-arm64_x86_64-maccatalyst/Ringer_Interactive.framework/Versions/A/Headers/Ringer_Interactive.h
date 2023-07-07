b2ff0225751ef92733eceba2934434e24898c29e
anhdang93 <nhocanh1993@gmail.com>
2023-07-07 14:34:56 +0700

    Release_1.0.4
    Release_1.0.4

---
+++ RingerInteractive.xcframework/ios-arm64_x86_64-maccatalyst/Ringer_Interactive.framework/Versions/A/Headers/Ringer_Interactive.h
@@ -0,0 +1,16 @@
+//
+//  Ringer_Interactive.h
+//  Ringer-Interactive
+//
+//  Created by AnhDNT on 27/08/2022.
+//
+
+#import <Foundation/Foundation.h>
+//! Project version number for Ringer_Interactive.
+FOUNDATION_EXPORT double Ringer_InteractiveVersionNumber;
+
+//! Project version string for Ringer_Interactive.
+FOUNDATION_EXPORT const unsigned char Ringer_InteractiveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ringer_Interactive/PublicHeader.h>
+
